3256be62ae3fd82d2caa18feb8789ee48b8ea44a
vinay021194 <vinaysaxena094@gmail.com>
2024-07-19 02:46:19 +0530

    some new modification

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/restaurants";
+// Use the environment variable for API URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getRestaurants = async () => {
   try {
